f2d409a0d27c729f719b3c00b310e15cb37457b7
emoyusa <emoyusa@gmail.com>
2014-06-07 17:33:04 -0400

    Help text added to UI.R

--- qiostate/UI.R
+++ qiostate/UI.R
@@ -2,7 +2,9 @@ shinyUI(fluidPage(
 headerPanel("Hospital Quality Measures by State, 2010"),
 sidebarLayout(position='right',
 sidebarPanel(
-selectInput("measure", "Choose a measure:",
+helpText("What is the quality of hospital care in your State compared with hospital care in other States? Are patients hospitalized 
+in your State more likely to receive important processes of care?  Are they less likely to die or experience adverse events?"),
+selectInput("measure", "Select a measure and find out:",
 choices = c("Heart Attack PCI","Heart Failure ACE","Heart Failure Discharge","Pneumonia Composite","Pneumonia Cultures","Pneumonia Dose 6 Hrs",
 "Pneumonia Right Antibiotic","Pneumonia Influenza","Pneumonia Pneumovax","Surgery Antibiotic Timing","Surgery Right Antibiotic",
 "Surgery Glucose Controlled","Surgery Hair Removed","Surgery Prevent Clots","Surgery Beta Blocker","Surgery Catheter Removed",
